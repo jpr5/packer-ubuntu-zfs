081bec8223fd971edd1dcbf60412df0aa3f2cfc9
Jordan Ritter <jpr5@darkridge.com>
2020-04-15 14:14:55 -0700

    Import focal-specific chroot-bootstrap script
    Main change is not writing anything to /etc/networks -- Focal uses
    CloudInit/NetPlan, seems to work automagically on its own.

---
+++ focal/scripts/chroot-bootstrap.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o pipefail
+set -o xtrace
+
+# Update APT with new sources
+apt-get update
+
+# Do not configure grub during package install
+echo 'grub-pc grub-pc/install_devices_empty select true' | debconf-set-selections
+echo 'grub-pc grub-pc/install_devices select' | debconf-set-selections
+
+# Install various packages needed for a booting system
+DEBIAN_FRONTEND=noninteractive apt-get install -y \
+    linux-aws \
+    grub-pc \
+    zfs-zed \
+    zfsutils-linux \
+    zfs-initramfs \
+    zfs-dkms \
+    gdisk
+
+# Set the locale to en_US.UTF-8
+locale-gen --purge "en_US.UTF-8"
+
+cat << EOF > /etc/default/locale
+LANG="en_US.UTF-8"
+LANGUAGE="en_US:en"
+EOF
+
+# Install OpenSSH
+apt-get install -y --no-install-recommends openssh-server
+
+# Install GRUB
+# shellcheck disable=SC2016
+sed -ri 's/GRUB_CMDLINE_LINUX=.*/GRUB_CMDLINE_LINUX="boot=zfs \$bootfs"/' /etc/default/grub
+grub-probe /
+grub-install /dev/xvdf
+
+# Configure and update GRUB
+mkdir -p /etc/default/grub.d
+cat << EOF > /etc/default/grub.d/50-aws-settings.cfg
+GRUB_RECORDFAIL_TIMEOUT=0
+GRUB_TIMEOUT=0
+GRUB_CMDLINE_LINUX_DEFAULT="console=tty1 console=ttyS0 ip=dhcp tsc=reliable net.ifnames=0"
+GRUB_TERMINAL=console
+EOF
+
+update-grub
+
+# Networking - Focal uses NetPlan/CloudInit, no need to configure anything now
+
+# Install standard packages
+DEBIAN_FRONTEND=noninteractive apt-get install -y ubuntu-standard
+DEBIAN_FRONTEND=noninteractive apt-get install -y cloud-init
